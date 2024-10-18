7cde18b56194fea8cd51313b9ddd0dae464196ef
Gaetan-bamongoyo <g.a.bamongoyo@gmail.com>
2024-10-18 10:34:55 +0200

    update compte

--- controllers/user.js
+++ controllers/user.js
@@ -2,42 +2,56 @@ const db = require('../sequelize/connector')
 
 const Users = db.user
 
-const addUsers = async (req, res)=>{
+const addUsers = async (req, res) => {
     const { email } = req.body;
-    const check = await Users.findOne({where: {email}});
+    const check = await Users.findOne({ where: { email } });
     let info = {
         nom: req.body.nom,
         email: req.body.email,
-        issuperuser: 0,
+        issuperuser: 1,
         motdepasse: req.body.motdepasse,
     }
-    if(check){
-        res.status(401).send({error: "Le compte existe deja"})
-    }else{
+    if (check) {
+        res.status(401).send({ error: "Le compte existe deja" })
+    } else {
         const user = await Users.create(info)
-        res.status(200).send(user)
+        res.status(200).send({ user, message: true })
     }
 }
 
-const verifyUser = async (req, res)=>{
-    const { email,motdepasse } = req.body;
-    if (!email){
+const verifyUser = async (req, res) => {
+    const { email, motdepasse } = req.body;
+    if (!email) {
         return res.status(400).send([])
-    }else{
-        try{
-            const user = await Users.findOne({where: {email, motdepasse}});
-            if(user){
-                res.status(200).send({user, message: true})
-            }else{
-                res.status(401).send({error: "Utilisateur n'existe"})
+    } else {
+        try {
+            const user = await Users.findOne({ where: { email, motdepasse } });
+            if (user) {
+                res.status(200).send({ user, message: true })
+            } else {
+                res.status(401).send({ error: "Utilisateur n'existe" })
             }
-        }catch(error){
-            res.status(500).send({error: "Erreur"})
+        } catch (error) {
+            res.status(500).send({ error: "Erreur" })
         }
     }
 }
 
-const getAll = async (req, res)=>{
+const modifierInformation = async (req, res) => {
+    let id = req.params.id
+    const { code } = req.body;
+    let info = {
+        issuperuser: 0,
+    }
+    if (code == "2024UVIRA") {
+        const user = await Users.update(info, { where: { id: id } })
+        res.status(200).send({ user, message: true })
+    }else{
+        res.status(404).send({message: false})
+    }
+}
+
+const getAll = async (req, res) => {
     const data = await Users.findAll({})
     res.status(200).send(data)
 }
@@ -45,5 +59,6 @@ const getAll = async (req, res)=>{
 module.exports = {
     addUsers,
     verifyUser,
-    getAll
+    getAll,
+    modifierInformation
 }
--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const router = require('express').Router()
 router.post('/add', userController.addUsers)
 router.post('/login', userController.verifyUser)
 router.get('/show', userController.getAll)
+router.put('/:id', userController.modifierInformation)
 
 module.exports = router
